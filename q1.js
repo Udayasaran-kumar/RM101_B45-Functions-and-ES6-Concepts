a239d2b3b6565b3fe801e38602890a0f5ae327af
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-05-06 13:02:26 +0530

    q1. added

--- q1.js
+++ q1.js
@@ -1,5 +1,4 @@
-const getOddSquares=(nums)=>{   
-    return (nums.filter(el=>el%2!==0).map((el)=>el*el).sort((a,b)=>a-b));
-}
+const getOddSquares=(nums)=>nums.filter(el=>el%2!==0).map((el)=>el*el).sort((a,b)=>a-b);
+
 let arr=[5, 2, 9, 4, 3];
 console.log(getOddSquares(arr));
